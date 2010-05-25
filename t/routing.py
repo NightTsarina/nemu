848aae5ff405110808b4e621391fc2d837b271ca
Martín Ferrari <mferrari@sophia.inria.fr>
2010-05-26 01:05:17 +0200

    placeholders

---
+++ t/routing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# vim:ts=4:sw=4:et:ai:sts=4
+
+import netns
+import unittest
+
+class TestRouting(unittest.TestCase):
+    def test_routing(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
