27b0be388839db1f6f46e3491b5fefd48ec7e525
Martín Ferrari <mferrari@sophia.inria.fr>
2010-06-24 00:37:47 +0200

    config.run_as attribute and get_nodes

--- src/netns/__init__.py
+++ src/netns/__init__.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 # vim:ts=4:sw=4:et:ai:sts=4
 
-import os
+import os, pwd
 from netns.node import Node
 
 class __Config(object):
     def __init__(self):
-        self.run_as = None
+        self._run_as = 65535
+        try:
+            self._run_as = pwd.getpwnam('nobody')[2]
+        except:
+            pass
+
+    def _set_run_as(self, uid):
+        if type(uid) != int:
+            uid = pwd.getpwnam(uid)[2]
+        if uid == 0:
+            raise AttributeError("Cannot run as root by default")
+        self._run_as = uid
+    def _get_run_as(self):
+        return self._run_as
+    run_as = property(_get_run_as, _set_run_as, None,
+            "Default uid to run applications as")
 
 config = __Config()
-__nodes = set()
-
-def get_nodes():
-    return set(__nodes)
+get_nodes = Node.get_nodes
 
 def set_cleanup_hooks(on_exit = False, on_signals = []):
     pass
