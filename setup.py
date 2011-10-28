e8a8a653bea8c94e91d7004d3de54869c34a9c48
alina <devnull@localhost>
2011-10-28 13:01:27 +0200

    removed validations from setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@
 from distutils.core import setup, Extension, Command
 import platform
 
-# CHECK dependencies
-# Linux kernel >= 2.6.36  
-l = platform.uname()[2].split("-")[0].split(".")
-l.reverse()
-if sum( int(l[i])*pow(10,i) for i in xrange(len(l))) < 296:
-    raise RuntimeError("Linux kernel >= 2.6.36 is required")
-
 setup(
         name        = 'netns',
         version     = '0.1',
