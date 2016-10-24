54089e863dce3daf9d615ce8cc377a01d8873fb7
Martín Ferrari <tincho@debian.org>
2016-10-24 05:11:39 +0200

    Cut release 0.3.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup, Extension, Command
 
 setup(
         name        = 'nemu',
-        version     = '0.2',
+        version     = '0.3',
         description = 'A lightweight network emulator embedded in a small '
                       'python library.',
         author      = 'Martín Ferrari, Alina Quereilhac',
